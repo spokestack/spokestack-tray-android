280b8452224a3b846c895675e5982e507da12996
space-pope <space-pope@users.noreply.github.com>
2020-10-01 19:59:58 +0000

    deploy: 636daa4805d8ad0d66a420859f30e3aadb4feb07

---
+++ scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":SpokestackTray:dokkaHtml/androidTestRelease": [], ":SpokestackTray:dokkaHtml/debug": [], ":SpokestackTray:dokkaHtml/main": [], ":SpokestackTray:dokkaHtml/release": []}'
